0ff89afce13dfc80cfc14bd2b3e2be949b132337
sasha <sashai123@users.noreply.github.com>
2025-09-11 20:04:38 -0400

    counts multiples of any integers a and/org b under an integer n, checked off items

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int n = 1000;
+        int a = 3;
+        int b = 5;
+        System.out.println(multiples(n, a, b));
+    }
+
+    public static int multiples(int n, int a, int b) {
         int counter = 0;
-        for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0)
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0)
             {
                 counter++;
             }
         }
-        System.out.println(counter);
+        return counter;
     }
 }
